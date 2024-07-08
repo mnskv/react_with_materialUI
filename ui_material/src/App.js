b0f2ceb966e611a873972519db3b01f3992c9fe2
Siva <siva.batta@ness.com>
2024-07-08 14:48:02 +0530

    Your commit message

--- ui_material/src/App.js
+++ ui_material/src/App.js
@@ -3,11 +3,14 @@ import Header from './components/Header';
 import MyCard from './components/CardUI/MyCard';
 import MyForm from './components/MyForm.jsx';
 import Card2 from './components/CardUI/Card2.js';
+import Message from './components/MessageApp/Message.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Message />
+
       
       <MyForm />
       <div className="card-container">
@@ -17,6 +20,7 @@ function App() {
       </div>
       
       <Card2 />
+      
       
     </div>
   );
